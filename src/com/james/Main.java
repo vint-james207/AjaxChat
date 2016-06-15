faec04103961e230bf0c131e727c0e57d471ffcd
jamesyburr <vint.james@gmail.com>
2016-06-15 12:01:44 -0400

    add put and delete routes

--- src/com/james/Main.java
+++ src/com/james/Main.java
@@ -62,5 +62,19 @@ public class Main {
                     return "";
                 }
         );
+        Spark.put(
+                "/edit-message",
+                (request, response) -> {
+                    //update message in database
+                    return "";
+                }
+        );
+        Spark.delete(
+                "/delete-message",
+                (request, response) -> {
+                    //delete message in database
+                    return "";
+                }
+        );
     }
 }
